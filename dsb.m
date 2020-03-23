5ac614790f34c8b6e0ebae57b9abc8970e9d87b9
AhmedAbdelaty2 <justgoand015@gmail.com>
2020-03-23 18:12:46 +0200

    changing the modulation system

--- dsb.m
+++ dsb.m
@@ -6,50 +6,39 @@ s = length(audio)/Fs;
 t = linspace(0, s, s*Fs+1);
 Fs1 = linspace(-Fs, Fs, s*Fs+1);
 
-%Y = fft(audio);
-%filteredSig = Y;
-%filteredSig(4001: end - 4001) = 0;
-
 d = designfilt('lowpassfir', 'Filterorder', 8000, 'CutoffFrequency', 2000, 'SampleRate', Fs);
-filteredSig = filter(d,audio);
+%filteredSig = filtfilt(d, audio);
+filteredSig = filter(d, audio);
 Y = fft(filteredSig);
-% sound(filteredSig, Fs);
+%sound(filteredSig, Fs);
 
-figure;
-subplot(2, 1, 1)
+figure;subplot(2, 1, 1)
 plot(t, audio);
 title('Before Filter')
 subplot(2, 1, 2);
 plot(t, filteredSig);
 title('After Filter');
 
-figure;
-subplot(2, 1, 1)
+figure;subplot(2, 1, 1)
 plot(Fs1, real(fftshift(fft(audio))));
 title('Before Filter')
 subplot(2, 1, 2);
 plot(Fs1, real(fftshift(Y)));
 title('After Filter');
 
-% audiowrite('filteredSig.wav', real(ifft(filteredSig)), Fs);
-filteredSig = ifft(filteredSig);
+%audiowrite('filteredSig.wav', real(ifft(filteredSig)), Fs);
+%filteredSig = real(ifft(filteredSig));
 
-% mse = sqrt(filteredSig - audio);
+%mse = sqrt(filteredSig - audio);
 
-Fc = 20e3;
+Fc = 100e3;
+Fs = 5*Fc;
+C = cos(2*pi*Fc*t);
+A = 515*2;
 
-message = filteredSig;
-
-suppressedCarrier = modulate(fft(message), Fc, Fs, 'am');
-transCarrier = modulate(fft(message), Fc, Fs, 'amdsb-tc');
-
-figure;
-subplot(2, 1, 1);
-plot(Fs1, real(fftshift(fft(suppressedCarrier))));
-title('DSB-SC');
-subplot(2, 1, 2);
-plot(Fs1, real(fftshift(fft(transCarrier))));
-title('DSB-TC');
-xlimit(-1, 1);
-
-%مساء الخير من قلب الورديان
+%DSB-SC
+ModuledSig = filteredSig.*transpose(C);
+figure; subplot(2,1,1);
+plot(t, ModuledSig);
+K = real(fftshift(fft(ModuledSig)));
+subplot(2,1,2); plot(Fs, K);
